97a21f5f129680b85358e802061dc417620cf0ed
Poppeloppen <victor.popp.henriksen@gmail.com>
2023-11-26 22:56:11 +0100

    Added solution to alternate and partially to many

--- redScare.py
+++ redScare.py
@@ -1,6 +1,8 @@
 from GraphTraversal.BFS import BFS
-from Utilities.ReadGraphExample import NoneGraphCreator
+from Utilities.ReadGraphExample import NoneGraphCreator, AlternateGraphCreator, GraphCreator
 from Utilities.Graph import Graph, DiGraph
+from Utilities.isDag import is_dag
+from DynamicProgramming.DAG_longest_path import dp_longest_path
 
 from typing import Union
 
@@ -18,7 +20,7 @@ def none(file_path: str) -> int:
 
 
 
-def some() -> Union[bool, str]:
+def some(file_path: str) -> Union[bool, str]:
     #G = SomeGraphCreator()
 
     if is_dag(G):
@@ -46,16 +48,26 @@ def some() -> Union[bool, str]:
     return
 
 
-def many() -> Union[bool, str]:
-    #G = ManyGraphCreator()
+def many(file_path: str) -> Union[bool, str]:
+    G_many = GraphCreator(file_path)
+    name_to_idx = G_many.index_of_vertex 
 
-    #if is_dag(G):
-        #return = dp_longest_path(G)
+    target_idx = name_to_idx[G_many.target]
 
-    return #"?!"
+    
+    #Only do dp in case of a DAG
+    if is_dag(G_many.G):
+        #Convert from dict{vertex_name: red_status} to dict{vertex_idx: red_status}
+        red_check = {name_to_idx[vertex_name]:(1 if v == 1 else 0) for vertex_name,v in G_many.is_vertex_red.items()}    
+
+        longest_path = dp_longest_path(G=G_many.G, is_vertex_red=red_check, target=target_idx)
+
+        return longest_path #dp_longest_path(G)
+
+    return "?!"
 
 
-def few() -> int:
+def few(file_path: str) -> int:
     #G = FewGraphCreator()
 
     #res = Dijkstra(G)
@@ -63,12 +75,17 @@ def few() -> int:
     return #res
 
 
-def alternate() -> bool:
-    #G = AlternateGraphCreator()
+def alternate(file_path: str) -> bool:
+    alternate_graph_obj = AlternateGraphCreator(file_path)
 
-    bfs = BFS(G=G, source=source, target=target)
+    G_alt = alternate_graph_obj.G
+    
+    source = alternate_graph_obj.source_idx
+    target = alternate_graph_obj.target_idx
 
-    return bfs.has_path_to(target=target)
+    bfs = BFS(G=G_alt, source=source, target=target)
+
+    return bfs.has_path_to(target)
 
 
 
@@ -83,14 +100,23 @@ def main():
                     "P3.txt",
                     "rusty-1-17.txt",
                     "ski-illustration.txt",
-                    "wall-n-10000.txt"
+                    #"wall-n-10000.txt"
                 ]
 
     for file_name in grap_files:
         print(file_name)
         path = f"./data/Graphs/{file_name}"
 
-        print(none(path))
+        #print("none:", none(path)) #DONE 
+
+        print("many:", many(path)) #INCOMPLETE
+
+        #print("alternate:", alternate(path)) #DONE
+    
+
+
+        print()
+
     return
 
 
