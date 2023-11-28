78a5247e65680960c1b0c9bc4d17e60cb76ffe7c
Poppeloppen <victor.popp.henriksen@gmail.com>
2023-11-28 15:44:18 +0100

    renamed DP for finding max red count

--- Utilities/DP_max_red.py
+++ Utilities/DP_max_red.py
@@ -2,10 +2,11 @@ import sys
 sys.path.append("../")
 from Utilities.Graph import Graph, DiGraph
 from Utilities.ReadGraphExample import GraphCreator
+from Utilities.isDag import is_dag
 
 
 
-def dp_longest_path(G: Graph, is_vertex_red: dict, target: int) -> int:
+def dp_max_reds(G: Graph, is_vertex_red: dict, target: int) -> int:
     M = [-1] * G.number_of_vertices
 
     M[0] = is_vertex_red[0]
@@ -15,7 +16,7 @@ def dp_longest_path(G: Graph, is_vertex_red: dict, target: int) -> int:
         #print(idx1, vertex1)
         for vertex2 in vertex1.keys():
             #print(vertex2)
-            M[vertex2] = max(M[idx1] + is_vertex_red[idx1],
+            M[vertex2] = max(M[idx1] + is_vertex_red[vertex2],
                                 M[vertex2])
     
     return M[target]
@@ -24,10 +25,10 @@ def dp_longest_path(G: Graph, is_vertex_red: dict, target: int) -> int:
 
 def main():
     grap_files = ["G-ex.txt",
-                    "grid-5-0.txt",
+                    #"grid-5-0.txt",
                     "increase-n8-1.txt",
-                    "P3.txt",
-                    "rusty-1-17.txt",
+                    #"P3.txt",
+                    #"rusty-1-17.txt",
                     "ski-illustration.txt",
                     #"wall-n-10000.txt"
                 ]
@@ -36,17 +37,27 @@ def main():
         print(file_name)
         path = f"../data/Graphs/{file_name}"
 
-        G_many = GraphCreator(path)
-        target = G_many.index_of_vertex[G_many.target]
-        #print(G_many.G)
-        #print(G_many.is_vertex_red)
-        red_check = {G_many.index_of_vertex[vertex_name]:(1 if v == 1 else 0) for vertex_name,v in G_many.is_vertex_red.items()}
+        G_data = GraphCreator(path)
+        G_data.add_edges()
+
+        G = G_data.G
+
+        print("type:", type(G))
+        print("is DAG?:", is_dag(G))
+        
+        
+        target_idx = G_data.index_of_vertex[G_data.target]
+        print("idx of target:", target_idx)
+        
+        red_check = {G_data.index_of_vertex[vertex_name]:(1 if v == 1 else 0) for vertex_name,v in G_data.is_vertex_red.items()}
 
 
         #print(red_check[0])
         #print("TEST")
-        res = dp_longest_path(G=G_many.G, is_vertex_red=red_check, target=target)
-        print(res)
+        res = dp_max_reds(G=G_data.G, is_vertex_red=red_check, target=target_idx)
+        print("max red cnt:", res)
+
+        print()
 
     return
 
