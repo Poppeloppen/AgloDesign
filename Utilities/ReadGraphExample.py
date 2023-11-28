e9d879183ce42fc1726367b6be3e34ca445e4472
Poppeloppen <victor.popp.henriksen@gmail.com>
2023-11-28 15:46:18 +0100

    class for creating "few" graph + started on "some"

--- Utilities/ReadGraphExample.py
+++ Utilities/ReadGraphExample.py
@@ -21,7 +21,9 @@ class GraphCreator():
 
         self.G = self._create_graph_object()
         self.add_vertices()
-        self.add_edges()
+        
+        #TODO: make sure to call this function explicitly in the files where GraphCreator is used
+        #self.add_edges()
 
 
     def _read_graph_data(self, file_path: str) -> None:
@@ -181,6 +183,87 @@ class AlternateGraphCreator(GraphCreator):
 
 
 
+class FewGraphCreator(GraphCreator):
+    def __init__(self, file_path: str) -> None:
+        super().__init__(file_path)
+
+        self.add_edges()#vertex_to_connect_to_source = red_vertex)
+
+        self.source_idx = self.index_of_vertex[self.source]
+        self.target_idx = self.index_of_vertex[self.target]
+
+    def add_edges(self) -> None:
+        """Add large weight to edges going to red vertices"""
+        
+        for edge in self.all_edges:
+            vertex_from, _, vertex_to = edge.split()         
+
+            vertex_from_idx = self.index_of_vertex[vertex_from]
+            vertex_to_idx = self.index_of_vertex[vertex_to]
+
+            #Penalize edges going to red vertices
+            if self.is_vertex_red[vertex_to]:
+                self.G.add_edge(u=vertex_from_idx, v=vertex_to_idx, w=10**7)
+            
+            else:
+                self.G.add_edge(u=vertex_from_idx, v=vertex_to_idx, w=1)
+
+                        
+        return
+
+
+
+
+
+class SomeGraphCreator(GraphCreator):
+    def __init__(self, file_path: str) -> None:
+        super().__init__(file_path)
+
+        self.create_additional_vertices()
+        self.add_edges()
+
+        self.source_idx = self.index_of_vertex[self.source]
+        self.target_idx = self.index_of_vertex[self.target]
+        
+    
+    def create_additional_vertices(self) -> None:
+        """Double the number of vertices + 2 to facilitate splitting each vertex
+        and adding source + sink vertices.
+        """
+        for _ in range(self.number_of_vertices + 2):
+            self.G.add_vertex()
+        
+        return
+
+
+    def add_edges(self):#, vertex_to_connect_to_source: int) -> None:
+        
+        #from v_1_in -> v_1_out and v_1_out->(v_n_in)
+        for edge in self.all_edges:
+            vertex_from, _, vertex_to = edge.split()         
+
+            vertex_from_idx = self.index_of_vertex[vertex_from]
+            vertex_to_idx = self.index_of_vertex[vertex_to]
+
+            print(f"{edge}: ({vertex_from_idx}, {vertex_to_idx})")
+
+            #v_1_in -> v_1_out
+            self.G.add_edge(u=vertex_from_idx, v=vertex_from_idx+self.number_of_vertices)
+
+            
+            
+
+        #from  new_source -> some_red_vertex (capacity: 2)
+
+
+        #from (old_target, old_source) -> new_target (capacity: 1 (each))
+
+        return
+    
+
+
+
+
 def main():
     grap_files = ["G-ex.txt",
                 "grid-5-0.txt",
@@ -196,18 +279,22 @@ def main():
         path = f"../data/Graphs/{file_name}"
 
         #G = GraphCreator(path)
-        #print(G.all_vertices)
-        #print(G.red_vertices)
 
         #G_none = NoneGraphCreator(path)
-        #G_none.add_vertices()
-        #G_none.add_edges()
 
-        G_alt = AlternateGraphCreator(path)
-        #G_alt.add_vertices()
-        print(G_alt.G)
-        print(G_alt.index_of_vertex)
-        #print(G_alt.vertex_of_index)
+        #G_alt = AlternateGraphCreator(path)
+        
+        #G_few = FewGraphCreator(path)
+        #print(G_few.G)
+
+        G_some = SomeGraphCreator(path)
+        
+        #TODO: I need to iterate over this list and run the FordFulkerson for each value
+        #indices_of_reds = [index for index, value in enumerate(G_some.red_vertices) if value == 1]
+
+        #print(indices_of_reds)
+        
+        print(G_some.G)
 
         print()
     return
